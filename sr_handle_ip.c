d5b1d75fe81900e1e4cd7ae619bf2fcc0487880d
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2014-12-06 04:06:37 -0800

    Return to sr_modify_and_send_icmp for simplicity reaons

--- sr_handle_ip.c
+++ sr_handle_ip.c
@@ -32,8 +32,7 @@ void sr_handle_ip(struct sr_instance *sr, uint8_t *packet, unsigned int len, str
 
 void sr_handle_ip_rec(struct sr_instance *sr, uint8_t *packet, unsigned int len, struct sr_if *iface) {
   Debug("Got IP packet: ");
-  // Extract ethernet and IP hdrs
-  //sr_ethernet_hdr_t *eth_hdr = packet_get_eth_hdr(packet);
+
   sr_ip_hdr_t *ip_hdr = packet_get_ip_hdr(packet);
 
   // Get IP protocol information
@@ -45,7 +44,7 @@ void sr_handle_ip_rec(struct sr_instance *sr, uint8_t *packet, unsigned int len,
     case ip_protocol_udp:
       Debug("TCP/UDP request received, sending port unreachable\n");
       // Send ICMP port unreachable
-      sr_send_icmp_to(sr, packet, icmp_protocol_type_dest_unreach,
+      sr_send_icmp_t3_to(sr, packet, icmp_protocol_type_dest_unreach,
           icmp_protocol_code_port_unreach, iface);
       break;
     // If it is an ICMP packet...
@@ -63,8 +62,8 @@ void sr_handle_ip_rec(struct sr_instance *sr, uint8_t *packet, unsigned int len,
           icmp_hdr->icmp_code == icmp_protocol_code_empty) {
         Debug("Got an echo (ping) request, responding with reply\n");
         // Send ICMP echo reply
-        sr_send_icmp_to(sr, packet, icmp_protocol_type_echo_rep,
-            icmp_protocol_type_echo_rep, iface);
+        sr_modify_and_send_icmp(sr, icmp_protocol_type_echo_rep,
+            icmp_protocol_type_echo_rep, packet, len, iface);
       }
       break;
     default:
@@ -75,7 +74,7 @@ void sr_handle_ip_rec(struct sr_instance *sr, uint8_t *packet, unsigned int len,
 
 // Sends an ICMP error message from sr out of interface iface
 // to receiver noted in the uint8_t receiver IP packet.
-int sr_send_icmp_to(struct sr_instance *sr, uint8_t *receiver,
+int sr_send_icmp_t3_to(struct sr_instance *sr, uint8_t *receiver,
     uint8_t icmp_type, uint8_t icmp_code, struct sr_if *iface) {
 
   unsigned int len = sizeof(sr_ethernet_hdr_t) +
@@ -120,5 +119,26 @@ int sr_send_icmp_to(struct sr_instance *sr, uint8_t *receiver,
       sizeof(sr_icmp_t3_hdr_t)); // cksum of ICMP
 
   int res = sr_send_packet(sr, packet, len, iface->name);
+  return res;
+}
+
+int sr_modify_and_send_icmp(struct sr_instance *sr, uint8_t icmp_type, uint8_t icmp_code, uint8_t *packet, int len, struct sr_if *iface) {
+  sr_ethernet_hdr_t *eth_hdr = packet_get_eth_hdr(packet);
+  sr_ip_hdr_t *ip_hdr = packet_get_ip_hdr(packet);
+  sr_icmp_hdr_t *icmp_hdr = packet_get_icmp_hdr(packet);
+
+  memcpy(eth_hdr->ether_dhost, eth_hdr->ether_shost, ETHER_ADDR_LEN);
+  memcpy(eth_hdr->ether_shost, iface->addr, ETHER_ADDR_LEN);
+
+  uint32_t req_src = ip_hdr->ip_src;
+  ip_hdr->ip_src = iface->ip; // src is this interface's ip
+  ip_hdr->ip_dst = req_src; // dst is requester's ip
+
+  icmp_hdr->icmp_type = icmp_type;
+  icmp_hdr->icmp_code = icmp_code;
+  icmp_hdr->icmp_sum = cksum(icmp_hdr,
+      sizeof(sr_icmp_hdr_t)); // cksum of ICMP
+  int res = sr_send_packet(sr, packet, len, iface->name);
+
   return res;
 }
