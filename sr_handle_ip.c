eed86a6b7c1aa7944c4ab1e28c3015ecd8068555
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2014-12-06 03:37:48 -0800

    Remove function modify_and_send_icmp in favor of more explicit send_icmp_t3_to

--- sr_handle_ip.c
+++ sr_handle_ip.c
@@ -63,8 +63,10 @@ void sr_handle_ip_rec(struct sr_instance *sr, uint8_t *packet, unsigned int len,
           icmp_hdr->icmp_code == icmp_protocol_code_empty) {
         Debug("Got an echo (ping) request, responding with reply\n");
         // Send ICMP echo reply
-        sr_modify_and_send_icmp(sr, icmp_protocol_type_echo_rep,
-            icmp_protocol_type_echo_rep, packet, len, iface);
+        //sr_modify_and_send_icmp(sr, icmp_protocol_type_echo_rep,
+        //    icmp_protocol_type_echo_rep, packet, len, iface);
+        sr_send_icmp_t3_to(sr, packet, icmp_protocol_type_echo_rep,
+            icmp_protocol_type_echo_rep, iface);
       }
       break;
     default:
@@ -107,7 +109,7 @@ int sr_send_icmp_t3_to(struct sr_instance *sr, uint8_t *receiver,
   ip_hdr->ip_v = rec_ip_hdr->ip_v;
   ip_hdr->ip_src = iface->ip;
   ip_hdr->ip_dst = rec_ip_hdr->ip_src;
-  ip_hdr->ip_len = htons(sizeof(sr_ip_hdr_t) + sizeof(sr_icmp_t3_hdr_t));
+  ip_hdr->ip_len = htons(len - sizeof(sr_ethernet_hdr_t));
   ip_hdr->ip_sum = 0;
   ip_hdr->ip_sum = cksum((const void *)ip_hdr,
       sizeof(sr_ip_hdr_t));
@@ -120,26 +122,5 @@ int sr_send_icmp_t3_to(struct sr_instance *sr, uint8_t *receiver,
       sizeof(sr_icmp_t3_hdr_t)); // cksum of ICMP
 
   int res = sr_send_packet(sr, packet, len, iface->name);
-  return res;
-}
-
-int sr_modify_and_send_icmp(struct sr_instance *sr, uint8_t icmp_type, uint8_t icmp_code, uint8_t *packet, int len, struct sr_if *iface) {
-  sr_ethernet_hdr_t *eth_hdr = packet_get_eth_hdr(packet);
-  sr_ip_hdr_t *ip_hdr = packet_get_ip_hdr(packet);
-  sr_icmp_hdr_t *icmp_hdr = packet_get_icmp_hdr(packet);
-
-  memcpy(eth_hdr->ether_dhost, eth_hdr->ether_shost, ETHER_ADDR_LEN);
-  memcpy(eth_hdr->ether_shost, iface->addr, ETHER_ADDR_LEN);
-
-  uint32_t req_src = ip_hdr->ip_src;
-  ip_hdr->ip_src = iface->ip; // src is this interface's ip
-  ip_hdr->ip_dst = req_src; // dst is requester's ip
-
-  icmp_hdr->icmp_type = icmp_type;
-  icmp_hdr->icmp_code = icmp_code;
-  icmp_hdr->icmp_sum = cksum(icmp_hdr,
-      sizeof(sr_icmp_hdr_t)); // cksum of ICMP
-  int res = sr_send_packet(sr, packet, len, iface->name);
-
   return res;
 }
--- sr_handle_ip.h
+++ sr_handle_ip.h
@@ -7,8 +7,6 @@ void sr_handle_ip(struct sr_instance* sr, uint8_t *packet, unsigned int len, str
 // Handle all IP datagrams meant for router
 void sr_handle_ip_rec(struct sr_instance *sr, uint8_t *packet, unsigned int len, struct sr_if *iface);
 
-int sr_modify_and_send_icmp(struct sr_instance *sr, uint8_t icmp_type, uint8_t icmp_code, uint8_t *packet, int len, struct sr_if *iface);
-
 int sr_send_icmp_t3_to(struct sr_instance *sr, uint8_t *receiver, uint8_t icmp_type, uint8_t icmp_code, struct sr_if *iface);
 
 int sr_send_icmp_to(struct sr_instance *sr, uint8_t *receiver, uint8_t icmp_type, uint8_t icmp_code, struct sr_if *iface);
